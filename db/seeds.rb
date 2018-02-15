0da20cd19e17d3c4c3772948531c58f4015d3bbf
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-02-15 12:16:54 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+require 'open-uri'
+require 'json'
+
+puts "Seeding ingredients"
+
+json = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+my_hash = JSON.parse(json)
+
+my_hash["drinks"].each do |pair|
+  ingredient = pair.values[0]
+  Ingredient.create(name: ingredient)
+end
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+puts "Finished"
